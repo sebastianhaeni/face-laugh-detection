d1d7fb7cc14eac1e0e6f7e679aa173feb17fc1a2
raeffu <raeffu@raeffu.ch>
2016-11-01 19:55:22 +0100

    first identification version

--- FaceIdentifier.m
+++ FaceIdentifier.m
@@ -2,9 +2,18 @@ clear all; close all; clc; %clear matrices, close figures & clear cmd wnd.
 
 % filename = '/Users/rlaubscher/Dropbox/BFH/CPVR2-3-CP/Exercises/Images/cpvr_faces_160/0000/01.JPG';
 
+searchFacePath = '~/Desktop/test.jpg';
+SI = imread(searchFacePath);
+searchFace = rgb2gray(SI);
+searchFace = double(searchFace)/255;
+figure;
+imshow(searchFace);
+title('Search Face');
+pause;
+
 %% Step 1: Load face images & convert each image into a vector of a matrix
 k = 0;
-for i=1:1:10
+for i=23:1:36
     for j=1:1:10
         filename  = sprintf('./images/cpvr_faces_160/00%2.2d/%2.2d.JPG',i,j);
         I = imread(filename);
@@ -25,4 +34,54 @@ for i=1:nImages
 end;
 figure('Color',[1 1 1]); 
 imshow(reshape(mn, imsize)); title('mean face');
+pause;
 
+%% Step 3: Calculate Eigenvectors & Eigenvalues
+% Create covariance matrix faster by using 
+% Turk and Pentland's trick to get the eigenvectors of faces*faces' from
+% the eigenvectors of faces'*faces
+tic;
+C = faces'*faces;
+[eigvec,eigval] = eig(C);
+eigvec = faces * eigvec;                        % Convert eigenvectors back as if they came from A'*A
+eigvec = eigvec / (sqrt(abs(eigval)));          % Normalize eigenvectors
+% eigvec & eigval are in fact sorted but in the wrong order
+eigval = diag(eigval);                          % Get the eigenvalue from the diagonal
+eigval = eigval / nImages;                      % Normalize eigenvalues
+[eigval, indices] = sort(eigval, 'descend');    % Sort the eigenvalues
+eigvec = eigvec(:, indices);                    % Sort the eigenvectors accordingly
+toc;
+
+% Display the 10 first eigenvectors as eigenfaces
+% figure('Color',[1 1 1]);
+% for n = 1:10
+%     subplot(4, 3, n);
+%     eigvecImg = reshape(eigvec(:,n), imsize);   % Reshape vector to image
+%     imshow(eigvecImg, []);                      % Show eigenface image with max. contrast
+% end
+% pause;
+%% Step 4: Transform the mean shifted faces into the faces2 space
+
+%########################
+faces2 = eigvec' * faces;
+%######################## 
+
+%% Step 6: Select an face to search in the PC space
+i = 41;                                         %index of face to be searched
+% searchFace = reshape(mn+faces(:,i), imsize);    %reshape from vector image
+search = eigvec' * (searchFace(:) - mn);        %transform into PC space
+
+% Calculate the squared euclidean distances to all faces in the PC space
+% We use the dot product to square the vector difference.
+for i=1:nImages
+    distPC(i) = dot(faces2(:,i)-search, faces2(:,i)-search);
+end;
+
+% Sort the distances and show the nearest 14 faces
+[sortedDistPC, sortIndex] = sort(distPC); % sort distances
+figure('Color',[1 1 1]);
+for i=1:6
+    subplot(2,3,i);  
+    imshow((reshape(mn+faces(:,sortIndex(i)), imsize))); 
+    title(sprintf('Dist=%2.2f',sortedDistPC(i)));
+end;
