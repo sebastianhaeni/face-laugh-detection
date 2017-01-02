35bb593c4440062c08aae02896c6c1aea41d205e
raeffu <raeffu@raeffu.ch>
2017-01-02 01:19:14 +0100

    mouth corner points

--- mouthDetector.m
+++ mouthDetector.m
@@ -1,6 +1,6 @@
 clear all; clear clc; close all;
 
-img = imread('/Users/rlaubscher/projects/bfh/face-laugh-detection/images/smile/negative/7.jpg');
+img = imread('/Users/rlaubscher/projects/bfh/face-laugh-detection/images/smile/test_positive_2.jpg');
 
 thresholdFace = 1;
 thresholdParts = 1;
@@ -23,3 +23,27 @@ figure;imshow(bbimg);
 for i=1:size(bbfaces,1)
  figure;imshow(bbfaces{i});
 end
+
+% Mouth image
+mouth = imcrop(img, bbox(5:8));
+figure; imshow(mouth);
+
+% smiling_factor = get_smile_factor(mouth);    % Determine smile amount
+
+% check for teeth
+
+% get_smile_teeth(mouth);
+
+bw = rgb2gray(mouth);
+corners = detectHarrisFeatures(bw);
+figure;imshow(bw); hold on;
+plot(corners.selectStrongest(50));
+
+coordinates = corners.selectStrongest(50).Location;
+left = coordinates(coordinates == min(coordinates(:,1)),:);
+right = coordinates(coordinates == max(coordinates(:,1)),:);
+bottom = coordinates(coordinates(:,2) == max(coordinates(:,2)), :);
+
+plot(left(:,1), left(:,2), '+r');
+plot(right(:,1), right(:,2), '+r');
+plot(bottom(:,1), bottom(:,2), '+r');
