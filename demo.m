12ce9107da87c08d18a2fc9ff780f3adbb3d66fe
Sebastian Häni <haeni.sebastian@gmail.com>
2017-01-15 20:29:13 +0100

    added loop

--- demo.m
+++ demo.m
@@ -2,18 +2,21 @@ clear all; clear clc; close all;
 
 addpath('lib');
 
-%I = snapshot(webcam(1));
-I = imread('images/smile/test_negative.jpg');
-F = face(I);
-M = mouth(F);
-S = smile(M);
+while true
+    I = snapshot(webcam(1));
+    F = face(I);
+    M = mouth(F);
+    S = smile(M);
 
-imshow(I);
+    imshow(I);
 
-if S > 0.7 
-    title('Sie sehen glücklich aus.');
-elseif S > 0.4
-   title('Ihre Stimmung scheint zu schwanken.');
-else
-    title('Sie sehen nicht sehr glücklich aus.');
+    if S > 0.7 
+        title('Sie sehen glücklich aus.');
+    elseif S > 0.4
+       title('Ihre Stimmung scheint zu schwanken.');
+    else
+        title('Sie sehen nicht sehr glücklich aus.');
+    end
+    
+    pause(1);
 end
